4e74bcea6599e28166c61b08c05640ab373bba12
Mike Leahy <mikel9286@hotmail.com>
2013-10-10 22:31:49 -0400

    update view loading

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 
 (function LoadPartialViews() {
-	//$('section.SocialIcons').load('SocialIcons.html');
 	$('div#portalContainer').load('views/Portals.html');
 	$('div#slideshowContainer').load('views/SlideShow.html');
-	Slideshow.Start;
+	Slideshow.Start();
 })();
